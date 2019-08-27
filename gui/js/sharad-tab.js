d1a4047b3b39d6b8ee6df4f8bfcc6eec3f2b02a8
Simon Schneegans <simon.schneegans@dlr.de>
2019-08-27 11:58:57 +0200

    :wrench: Remove obsolete JS classes

--- gui/js/sharad-tab.js
+++ gui/js/sharad-tab.js
@@ -2,7 +2,7 @@ function add_sharad(file, time) {
     const html = `
         <div class='row item-${file}''>
             <div class='col-8' >${file}</div>
-            <div class='col-4'><a class='waves-effect waves-light btn glass block' onclick='window.call_native("set_time", ${time})' ><i class='material-icons'>restore</i></a></div>
+            <div class='col-4'><a class='btn glass block' onclick='window.call_native("set_time", ${time})' ><i class='material-icons'>restore</i></a></div>
         </div>`;
 
     $('#list-sharad').append(html);
